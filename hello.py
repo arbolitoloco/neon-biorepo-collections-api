94e7cbd83418451342ab2de5cbd2645509413c1f
Laura Rocha Prado <lauraprado@asu.edu>
2020-03-31 13:38:33 -0700

    prints out file paths in travis

--- hello.py
+++ hello.py
@@ -3,24 +3,25 @@ import os, csv, json
 
 inputfile = os.path.abspath("data/categories.csv")
 outputfile = os.path.abspath("api/categories/all.json")
-
+print(inputfile)
+print(outputfile)
 # df = pd.read_csv (inputpath)
 # df.to_json (outputpath, orient='table')
 
 # Read the CSV and add data to a dictionary
-data = {}
-with open(inputfile) as csvFile:
-  csvReader = csv.DictReader(csvFile)
-  for csvRow in csvReader:
-    shortname = csvRow["shortname"]
-    data[shortname] = csvRow
-print(data)
+# data = {}
+# with open(inputfile) as csvFile:
+#   csvReader = csv.DictReader(csvFile)
+#   for csvRow in csvReader:
+#     shortname = csvRow["shortname"]
+#     data[shortname] = csvRow
+# print(data)
 
 # Add data to a root node
-root = {}
-root["categories"] = data
+# root = {}
+# root["categories"] = data
 
 # Write data to JSON file
-with open(outputfile, "w") as jsonFile:
-  jsonFile.write(json.dumps(root, indent=4))
-print("Attempted to create JSON file")
+# with open(outputfile, "w") as jsonFile:
+#   jsonFile.write(json.dumps(root, indent=4))
+# print("Attempted to create JSON file")
